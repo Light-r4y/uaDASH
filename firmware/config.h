5c7fc92d3e99038bb4c9ce9e10e4806008949091
Light-r4y <116795744+Light-r4y@users.noreply.github.com>
2025-06-14 19:59:21 +0300

    change to build for 5"

--- firmware/config.h
+++ firmware/config.h
@@ -1,8 +1,8 @@
 #ifndef DISPLAY_CONFIG_H
 #define DISPLAY_CONFIG_H
 
-// #define JC8048W550C             // JC8048W550C 5inch C-touch
-#define WAVESHARE_S3_LCD7       // Waveshare:ESP32-S3-Touch-LCD-7
+#define JC8048W550C             // JC8048W550C 5inch C-touch
+// #define WAVESHARE_S3_LCD7       // Waveshare:ESP32-S3-Touch-LCD-7
 // #define WAVESHARE_S3_LCD5       // Waveshare:ESP32-S3-Touch-LCD-5 800x480
 
 
