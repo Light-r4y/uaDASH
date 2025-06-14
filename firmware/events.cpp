13709cde8051952adffa37057c55dd882f1fa9a6
Light-r4y <116795744+Light-r4y@users.noreply.github.com>
2025-06-14 23:44:07 +0300

    add bench IGN + INJ (worked on 7")

--- firmware/events.cpp
+++ firmware/events.cpp
@@ -9,107 +9,102 @@ void downBribrightness(lv_event_t *e) {
   setBrightness(brightnessVal - 40);
 }
 
-uint8_t data[] = { 0x66, 0x00, 0x00, 0xBB };
+uint8_t data[] = { 0x66, 0x00, 0x00, 0x00, 0x00 };
+
+void bench(uint8_t subsys, uint8_t index) {
+  data[2] = subsys;
+  data[4] = index;
+  bool ret = can.send(0x77000C, data, sizeof(data), true);
+  char sys[8] = {0};
+  switch (subsys) {
+    case 0x12:
+      memcpy(&sys,"IGN", 3);
+      break;
+    case 0x13:
+      memcpy(&sys,"INJ", 3);
+      break;
+    default:
+      memcpy(&sys,"???", 3);
+  }
+  lv_label_set_text_fmt(ui_debugStatus, "bench%s%d send status: %s", sys, index, ret ? "OK" : "FAIL");
+}
 
 void benchIGN1(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN1 send status: %s", ret ? "OK" : "FAIL");
-#ifdef DEBUG
-  Serial.println("benchIGN1");
-#endif
+  bench(0x12, 1);
 }
 
 void benchIGN2(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN2 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 2);
 }
 
 void benchIGN3(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN3 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 3);
 }
 
 void benchIGN4(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN4 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 4);
 }
 
 void benchIGN5(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN5 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 5);
 }
 
 void benchIGN6(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN6 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 6);
 }
 
 void benchIGN7(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN7 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 7);
 }
 
 void benchIGN8(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchIGN8 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x12, 8);
 }
 
 void benchINJ1(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ1 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 1);
 }
 
 void benchINJ2(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ2 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 2);
 }
 
 void benchINJ3(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ3 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 3);
 }
 
 void benchINJ4(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ4 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 4);
 }
 
 void benchINJ5(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ5 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 5);
 }
 
 void benchINJ6(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ6 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 6);
 }
 
 void benchINJ7(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ7 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 7);
 }
 
 void benchINJ8(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchINJ8 send status: %s", ret ? "OK" : "FAIL");
+  bench(0x13, 8);
 }
 
 void StartStop(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "StartStop send status: %s", ret ? "OK" : "FAIL");
+
 }
 
 void benchFuelPump(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchFuelPump send status: %s", ret ? "OK" : "FAIL");
+
 }
 
 void benchFan2(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchFan2 send status: %s", ret ? "OK" : "FAIL");
+
 }
 
 void benchFan1(lv_event_t *e) {
-  bool ret = can.send(0x77000C, data, sizeof(data), true);
-  lv_label_set_text_fmt(ui_debugStatus, "benchFan1 send status: %s", ret ? "OK" : "FAIL");
+
 }
