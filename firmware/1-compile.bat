1ab88c8e2369db8e9f871fc5968bbd1369d8f0b5
Light-r4y <116795744+Light-r4y@users.noreply.github.com>
2025-06-27 22:34:28 +0300

    update compile.bat

--- firmware/1-compile.bat
+++ firmware/1-compile.bat
@@ -1,4 +1,4 @@
 arduino-cli -j16 compile -b esp32:esp32:esp32s3:UploadSpeed=115200,FlashSize=16M,PartitionScheme=app3M_fat9M_16MB,PSRAM=opi firmware.ino -v
 
 
-rem arduino-cli -j16 compile -b esp32:esp32:esp32s3:FlashSize=16M,PartitionScheme=app3M_fat9M_16MB,PSRAM=opi,EventsCore=0 --build-property "compiler.cpp.extra_flags=-DJC8048W550C -O2" firmware.ino --output-dir ./artifacts -v
+rem arduino-cli -j16 compile -b esp32:esp32:esp32s3:FlashSize=16M,PartitionScheme=app3M_fat9M_16MB,PSRAM=opi,EventsCore=0 --build-property "build.defines=-DBOARD_HAS_PSRAM -D<display_define>" --build-property compiler.optimization_flags=-O2 firmware.ino --output-dir ./artifacts -v
