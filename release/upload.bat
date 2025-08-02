6f2a658438b9b5f593e330cbaef544b7c2733b2b
Light-r4y <116795744+Light-r4y@users.noreply.github.com>
2025-08-02 19:33:01 +0300

    Update upload.bat

--- release/upload.bat
+++ release/upload.bat
@@ -1,3 +1,4 @@
 @echo off
-.\esptool.exe --chip esp32s3 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode keep --flash_freq keep --flash_size keep 0x0 "esp32.esp32.esp32s3/firmware.ino.bootloader.bin" 0x8000 "esp32.esp32.esp32s3/firmware.ino.partitions.bin" 0xe000 "esp32.esp32.esp32s3/boot_app0.bin" 0x10000 "esp32.esp32.esp32s3/firmware.ino.bin"
+.\esptool.exe --chip esp32s3 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode keep --flash_freq keep --flash_size keep 0x0 "esp32.esp32.esp32s3/firmware.ino.bootloader.bin" 0x8000 "esp32.esp32.esp32s3/firmware.ino.partitions.bin" 0xe000 "./boot_app0.bin" 0x10000 "esp32.esp32.esp32s3/firmware.ino.bin"
+
 pause
