79799aaa35fab695c8aa94ea0a8b1e4e3bb04c9c
Light-r4y <urpek_xlll@google.com>
2025-11-22 21:00:30 +0300

    add gear change events

--- firmware/events.cpp
+++ firmware/events.cpp
@@ -144,7 +144,19 @@ void benchScreenInitSetup(lv_event_t *e)
 
 void ClickGearUp(lv_event_t *e)
 {
+  // uint8_t data[] = { 0x66, 0x00, 0x14, 0x00, 0x03, 0x00 };
+  // TS_TCU_UPSHIFT_REQUEST = 0x03,
+  uint8_t data[] = {(uint8_t)bench_test_magic_numbers_e::BENCH_HEADER, 0x00, 0x14, 0x00, 0x03, 0x00};
+  // 0x77000C 0x66 0x00 0x14 0x00 0x03 0x00 shift up
+  // bool ret = can.send(0x77000C, data, sizeof(data), true);
+  bool ret = can.send((uint32_t)bench_test_packet_ids_e::ECU_CAN_BUS_USER_CONTROL, data, sizeof(data), true);
 }
 void ClickGearDown(lv_event_t *e)
 {
+  // uint8_t data[] = { 0x66, 0x00, 0x14, 0x00, 0x04, 0x00 };
+  // TS_TCU_DOWNSHIFT_REQUEST = 0x04,
+  uint8_t data[] = {(uint8_t)bench_test_magic_numbers_e::BENCH_HEADER, 0x00, 0x14, 0x00, 0x04, 0x00};
+  // 0x77000C 0x66 0x00 0x14 0x00 0x04 0x00 shift down
+  // bool ret = can.send(0x77000C, data, sizeof(data), true);
+  bool ret = can.send((uint32_t)bench_test_packet_ids_e::ECU_CAN_BUS_USER_CONTROL, data, sizeof(data), true);
 }
